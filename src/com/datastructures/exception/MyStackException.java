54e3bbaa2e51a4eb4afbfcccebdf419764ddf94f
VijayPrakashReddyBillapati-BVPR <prakashvijay54@gmail.com>
2021-08-23 09:03:06 +0530

    Create MyStackException.java

---
+++ src/com/datastructures/exception/MyStackException.java
@@ -0,0 +1,23 @@
+package com.datastructures.exception;
+
+import java.util.Arrays;
+
+public class MyStackException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	String s;
+	public MyStackException(String s) {
+		this.s= s;
+	}
+
+	@Override
+	public String toString() {
+		return "MyStackException "+s;
+	}
+	
+
+}
